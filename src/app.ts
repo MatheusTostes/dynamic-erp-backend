73585864d571bf047967f828092948819b243911
Tostes <matheus_tostes@hotmail.com>
2024-12-27 05:29:20 -0300

    chore(deploy): change default port

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ app.get("/hello", (req, res) => {
 // Register TSOA Routes
 RegisterRoutes(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
